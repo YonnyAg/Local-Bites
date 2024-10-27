6938e99a11ec24402603acb91792f6354c21aa35
Yonathan Abello <iyonny1610@gmail.com>
2024-10-26 23:09:45 -0300

    backend config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,10 +78,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LocalBites',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # o 'localhost'
+        'PORT': '3306',      
     }
 }
+
+
 
 
 # Password validation
