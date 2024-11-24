592c9c021b0b0d11357f4b6b8bd1bd834313b64f
Yonathan Abello <iyonny1610@gmail.com>
2024-11-24 16:55:08 -0300

    onrender allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = ['local-bites-backend.onrender.com', 'localhost']
 GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY')
 
 
